15094ce8e7bd1ebb7a46c9c2e140cab16807a81e
Jorge Codina <jorge.codina@gmail.com>
2017-06-20 11:36:09 -0400

    stable version

---
+++ src/components/component.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class DatePicker extends Component{
+  render(){
+    return (
+      <h1>{ 'Hello World' }</h1>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './components/component.jsx';
+
+ReactDOM.render( 
+  <Component />, 
+  document.getElementById('root') );
